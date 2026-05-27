68da5f81aa7016a2334c3b04a4993e374a5e67c0
Nissi Prats <nissiprats.r6@gmail.com>
2026-05-27 16:44:29 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(`
+    <h1>Hola mundo desde PaaS</h1>
+  `);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
